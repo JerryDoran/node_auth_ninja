127c4538f4eb08ca58266ac8aa0109b5fe508678
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-08-18 08:48:20 -0400

    Added signup and signin forms

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+// const bcrypt = require('bcrypt');
 
 // Create schema
 const userSchema = new mongoose.Schema({
@@ -16,6 +17,13 @@ const userSchema = new mongoose.Schema({
     minlength: [6, 'Minimum password length is 6 characters'],
   },
 });
+
+// fire a function before doc saved to db
+// userSchema.pre('save', async function (next) {
+//   const salt = await bcrypt.genSalt();
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
 
 const User = mongoose.model('user', userSchema);
 
